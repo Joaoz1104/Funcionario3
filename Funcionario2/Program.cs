3a581c41eb9ec4c61095c89821089ff57bf4723c
Johnnyboi3020 <joaovitorzampier09@gmail.com>
2021-08-05 14:52:16 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionario2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Funcionario2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salario s = new Salario();
+            Console.WriteLine("Dados do primeiro funcionário:");
+
+            Console.WriteLine("Nome:");
+            s.Nome1 = Console.ReadLine();
+
+            Console.WriteLine("Salário:");
+            s.Salario1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.WriteLine("Nome:");
+            s.Nome2 = Console.ReadLine();
+
+            Console.WriteLine("Salário:");
+            s.Salario2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(s);
+
+        }
+    }
+}
---
+++ Funcionario2/Salario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Funcionario2
+{
+    class Salario
+    {
+        public string Nome1 { get; set; }
+        private int Salario1 { get; set; }
+        public string Nome2 { get; set; }
+        private int Salario2 { get; set; }
+
+        public int MediaSalario()
+        {
+            int Media = (Salario1 + Salario2) / 2;
+            return Media;
+        }
+        public override string ToString()
+        {
+            return $"Salário médio : {MediaSalario():F2}";
+        }
+    }
+}
